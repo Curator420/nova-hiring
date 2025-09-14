76922c031498c8ba571258afbc4af05b8e4cd98b
Prashant Joshi <60182914+Curator420@users.noreply.github.com>
2025-09-15 02:43:40 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-  `${import.meta.env.VITE_API_URL}/login`,
+  `${import.meta.env.VITE_API_URL}/api/v1/users/login`,
   { email, password, role },
   {
     headers: { "Content-Type": "application/json" },
