d155dcee1f8454245a1501d0e20fe950b3960df4
Prashant Joshi <60182914+Curator420@users.noreply.github.com>
2025-09-15 01:45:49 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,10 +1,14 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
+
+  // Ensure COOKIE_EXPIRE is a number
+  const cookieExpireDays = Number(process.env.COOKIE_EXPIRE);
+
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true, // Set httpOnly to true
+    expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // HTTPS only in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
